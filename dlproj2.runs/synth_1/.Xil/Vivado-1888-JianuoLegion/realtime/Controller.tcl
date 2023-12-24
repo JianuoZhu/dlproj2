b44c93f43e6af45247f8bb45c56f30a5389fa703
JianuoZhu <jianuo.zhu@gmail.com>
2023-12-24 20:20:44 +0800

    commit a update in test user function

--- dlproj2.runs/synth_1/.Xil/Vivado-1888-JianuoLegion/realtime/Controller.tcl
+++ dlproj2.runs/synth_1/.Xil/Vivado-1888-JianuoLegion/realtime/Controller.tcl
@@ -9,29 +9,25 @@ set rt::rc [catch {
   uplevel #0 {
     set ::env(BUILTIN_SYNTH) true
     source $::env(HRT_TCL_PATH)/rtSynthPrep.tcl
-    rt::HARTNDb_resetJobStats
-    rt::HARTNDb_resetSystemStats
-    rt::HARTNDb_startSystemStats
     rt::HARTNDb_startJobStats
     set rt::cmdEcho 0
     rt::set_parameter writeXmsg true
     rt::set_parameter enableParallelHelperSpawn true
     set ::env(RT_TMP) "E:/OneDrive/Code/FPGA/dlproj2/dlproj2.runs/synth_1/.Xil/Vivado-1888-JianuoLegion/realtime/tmp"
     if { [ info exists ::env(RT_TMP) ] } {
-      file delete -force $::env(RT_TMP)
       file mkdir $::env(RT_TMP)
     }
 
     rt::delete_design
 
     set rt::partid xc7a35tcsg324-1
-    source $::env(HRT_TCL_PATH)/rtSynthParallelPrep.tcl
 
     set rt::multiChipSynthesisFlow false
     source $::env(SYNTH_COMMON)/common.tcl
     set rt::defaultWorkLibName xil_defaultlib
 
-    set rt::useElabCache false
+    # Skipping read_* RTL commands because this is post-elab optimize flow
+    set rt::useElabCache true
     if {$rt::useElabCache == false} {
       rt::read_verilog {
       E:/OneDrive/Code/FPGA/dlproj2/DigitalDesignProject.srcs/sources_1/new/Auto_Play.v
@@ -43,18 +39,19 @@ set rt::rc [catch {
     }
       rt::filesetChecksum
     }
-    rt::set_parameter usePostFindUniquification false
+    rt::set_parameter usePostFindUniquification true
+    set rt::SDCFileList E:/OneDrive/Code/FPGA/dlproj2/dlproj2.runs/synth_1/.Xil/Vivado-1888-JianuoLegion/realtime/Controller_synth.xdc
+    rt::sdcChecksum
     set rt::top Controller
     set rt::reportTiming false
-    rt::set_parameter elaborateOnly true
-    rt::set_parameter elaborateRtl true
-    rt::set_parameter eliminateRedundantBitOperator false
+    rt::set_parameter elaborateOnly false
+    rt::set_parameter elaborateRtl false
+    rt::set_parameter eliminateRedundantBitOperator true
     rt::set_parameter writeBlackboxInterface true
+    rt::set_parameter ramStyle auto
     rt::set_parameter merge_flipflops true
-    rt::set_parameter srlDepthThreshold 3
-    rt::set_parameter rstSrlDepthThreshold 4
 # MODE: 
-    rt::set_parameter webTalkPath {}
+    rt::set_parameter webTalkPath {E:/OneDrive/Code/FPGA/dlproj2/dlproj2.cache/wt}
     rt::set_parameter enableSplitFlowPath "E:/OneDrive/Code/FPGA/dlproj2/dlproj2.runs/synth_1/.Xil/Vivado-1888-JianuoLegion/"
     set ok_to_delete_rt_tmp true 
     if { [rt::get_parameter parallelDebug] } { 
@@ -63,16 +60,23 @@ set rt::rc [catch {
     if {$rt::useElabCache == false} {
         set oldMIITMVal [rt::get_parameter maxInputIncreaseToMerge]; rt::set_parameter maxInputIncreaseToMerge 1000
         set oldCDPCRL [rt::get_parameter createDfgPartConstrRecurLimit]; rt::set_parameter createDfgPartConstrRecurLimit 1
-      rt::run_rtlelab -module $rt::top
+      rt::run_synthesis -module $rt::top
         rt::set_parameter maxInputIncreaseToMerge $oldMIITMVal
         rt::set_parameter createDfgPartConstrRecurLimit $oldCDPCRL
     }
 
     set rt::flowresult [ source $::env(SYNTH_COMMON)/flow.tcl ]
     rt::HARTNDb_stopJobStats
+    rt::HARTNDb_reportJobStats "Synthesis Optimization Runtime"
+    rt::HARTNDb_stopSystemStats
     if { $rt::flowresult == 1 } { return -code error }
 
 
+  set hsKey [rt::get_parameter helper_shm_key] 
+  if { $hsKey != "" && [info exists ::env(BUILTIN_SYNTH)] && [rt::get_parameter enableParallelHelperSpawn] && [info exists rt::doParallel] && $rt::doParallel} { 
+     $rt::db killSynthHelper $hsKey
+  } 
+  rt::set_parameter helper_shm_key "" 
     if { [ info exists ::env(RT_TMP) ] } {
       if { [info exists ok_to_delete_rt_tmp] && $ok_to_delete_rt_tmp } { 
         file delete -force $::env(RT_TMP)
